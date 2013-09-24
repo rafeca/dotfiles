4c3e6b625a4d6ac9fd4ca591c7fd4cb6217f956d
Rafael de Oleza <rafeca@gmail.com>
2013-09-25 00:53:26 +0200

    Add sublime text javascript snippets

--- sublimetext3/configure.sh
+++ sublimetext3/configure.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+# Add subl CLI
 ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
 
+# Install Package Control
+cp "`pwd`/$(dirname $0)/Package Control.sublime-package" "$HOME/Library/Application Support/Sublime Text 3/Installed Packages/"
+
+# Link preferences file
 rm -f "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
 ln -s "`pwd`/$(dirname $0)/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
 
-cp "`pwd`/$(dirname $0)/Package Control.sublime-package" "$HOME/Library/Application Support/Sublime Text 3/Installed Packages/"
+# Link snippets
+ln -s "`pwd`/$(dirname $0)/snippets" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Snippets"
