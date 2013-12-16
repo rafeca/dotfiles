2b95b1ad656ecec0d7f20e9b5c25588db4f48164
Rafael de Oleza <rafeca@gmail.com>
2013-12-17 00:49:06 +0100

    Add new modules to homebrew

--- homebrew/configure.sh
+++ homebrew/configure.sh
@@ -13,8 +13,15 @@ fi
 brew update
 
 # Install some packages
-brew install wget ssh-copy-id coreutils hub tmux reattach-to-user-namespace
+brew install wget ssh-copy-id coreutils hub trash tmux reattach-to-user-namespace
 
 
 # Tell applications that use xcode-select where the GNU utils are located
 sudo xcode-select --switch /usr/bin
+
+# Install homebrew cask
+brew tap phinze/homebrew-cask
+brew install brew-cask
+
+# Install quicklook plugins
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json && qlmanage -r
