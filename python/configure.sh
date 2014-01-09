7ce8eee5924f9837c403e26bfaf21e123e38936f
Rafael de Oleza <rafeca@gmail.com>
2014-01-09 17:33:45 +0100

    Upgrade python setuptools version

--- python/configure.sh
+++ python/configure.sh
@@ -3,6 +3,8 @@
 # Install PIP
 sudo easy_install pip
 
+sudo pip install setuptools --upgrade
+
 # Install some eggs
 sudo pip install pygments
 sudo pip install http://closure-linter.googlecode.com/files/closure_linter-latest.tar.gz
