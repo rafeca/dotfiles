f007fd2bad31be1f71a04d82f0b97968852e9097
Rafael de Oleza <rafeca@gmail.com>
2014-02-05 16:52:47 +0100

    Change order of settings in ~/.zshrc

--- zsh/main.zsh
+++ zsh/main.zsh
@@ -1,7 +1,7 @@
 # initialize autocomplete here, otherwise functions won't be loaded
 
+source "$(dirname $0)/path.zsh"
 source "$(dirname $0)/config.zsh"
 source "$(dirname $0)/prompt.zsh"
-source "$(dirname $0)/path.zsh"
 source "$(dirname $0)/aliases.zsh"
 source "$(dirname $0)/folders.zsh"
