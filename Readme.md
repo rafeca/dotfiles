0cbc1e9920656f4a52a3242d65e294c5588390c5
Rafael de Oleza <rafeca@gmail.com>
2014-03-03 10:58:38 +0100

    Add LimeChat basic config

--- Readme.md
+++ Readme.md
@@ -13,8 +13,8 @@ into my regular development machine.
 First thing to do is to install the most basic software that I use. All those apps
 are free and can be downloaded and easily installed:
 
+* Google Chrome
 * Google Chrome Canary
-* Firefox Aurora
 * Sublime Text 3
 * Alfred
 * iTerm2
@@ -23,7 +23,7 @@ are free and can be downloaded and easily installed:
 * CyberDuck
 * VirtualBox
 * VideoLAN
-* Textual
+* LimeChat
 
 ## OSX configuration
 
---
+++ limechat/configure.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+defaults write net.limechat.LimeChat "Preferences.General.join_on_doubleclick" -bool true
+
+defaults write net.limechat.LimeChat "Preferences.Theme.input_font_name" -string "Menlo-Regular"
+defaults write net.limechat.LimeChat "Preferences.Theme.input_font_size" -int 14
+defaults write net.limechat.LimeChat "Preferences.Theme.log_font_name" -string "Menlo-Regular"
+defaults write net.limechat.LimeChat "Preferences.Theme.log_font_size" -int 14
+
+defaults write net.limechat.LimeChat "Preferences.Theme.name" -string "resource:Spring Night"
