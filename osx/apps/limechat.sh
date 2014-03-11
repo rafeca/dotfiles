bc45ffb8873c38906f35b027cff90cc7a640c8f0
Rafael de Oleza <rafeca@gmail.com>
2014-03-11 22:43:09 +0100

    Fix limechat configuration

--- osx/apps/limechat.sh
+++ osx/apps/limechat.sh
@@ -9,5 +9,7 @@ defaults write net.limechat.LimeChat "Preferences.Theme.input_font_name" -string
 defaults write net.limechat.LimeChat "Preferences.Theme.input_font_size" -int 14
 defaults write net.limechat.LimeChat "Preferences.Theme.log_font_name" -string "Menlo-Regular"
 defaults write net.limechat.LimeChat "Preferences.Theme.log_font_size" -int 14
+defaults write net.limechat.LimeChat "Preferences.Theme.override_input_font" -bool true
+defaults write net.limechat.LimeChat "Preferences.Theme.override_log_font" -bool true
 
 defaults write net.limechat.LimeChat "Preferences.Theme.name" -string "resource:Spring Night"
