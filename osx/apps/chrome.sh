f8359f9a2563dcab9119379b07a8a2e8ff7e3ecc
Rafael de Oleza <rafeca@gmail.com>
2014-03-18 14:06:56 +0100

    Add chrome icon to dock

--- osx/apps/chrome.sh
+++ osx/apps/chrome.sh
@@ -4,4 +4,5 @@ brew cask install google-chrome
 brew cask install google-chrome-canary
 
 # Add shortcut to Dock
+defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>$HOME/Applications/Google Chrome.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
 defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>$HOME/Applications/Google Chrome Canary.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
