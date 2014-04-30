69d0c73a1079e15ab4c162b63687af6a5320eb40
Rafael de Oleza <rafeca@gmail.com>
2014-04-30 18:03:00 +0200

    Add vim plugins

--- vim/configure.sh
+++ vim/configure.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 ln -s "`pwd`/$(dirname $0)/vimrc.symlink" ~/.vimrc
+
+mkdir -p ~/.vim/autoload
+mkdir -p ~/.vim/bundle
+
+# Configure pathogen and vundle
+cp "`pwd`/$(dirname $0)/pathogen.vim" ~/.vim/autoload
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
