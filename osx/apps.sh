d6326eb9956c5bfab4d637cd3d3c7d7f346dd260
Rafael Oleza Alomar <rafeca@gmail.com>
2015-01-10 17:45:11 +0100

    Add script for installing all the apps

---
+++ osx/apps.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+osx/apps/alfred.sh
+osx/apps/chrome.sh
+osx/apps/cyberduck.sh
+osx/apps/iterm2.sh
+osx/apps/limechat.sh
+osx/apps/quicklookplugins.sh
+osx/apps/slate.sh
+osx/apps/sublimetext3.sh
+osx/apps/transmission.sh
+osx/apps/vagrant.sh
+osx/apps/videolan.sh
