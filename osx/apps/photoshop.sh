73b403518b6cd9e9f43e336563b7f072ecc4fb9f
Rafael Oleza Alomar <rafeca@gmail.com>
2015-01-10 17:43:01 +0100

    Add support for Adobe Photoshop CC

--- osx/apps/photoshop.sh
+++ osx/apps/photoshop.sh
@@ -11,3 +11,9 @@ then
   # Link CS6 Workspace
   ln -s "$(pwd)/$(dirname $0)/photoshop/rafeca workspace CS6" "$HOME/Library/Preferences/Adobe Photoshop CS6 Settings/WorkSpaces/"
 fi
+
+if [ -d "$HOME/Library/Preferences/Adobe Photoshop CC Settings" ]
+then
+  # Link CC Workspace
+  ln -s "$(pwd)/$(dirname $0)/photoshop/rafeca workspace CS6" "$HOME/Library/Preferences/Adobe Photoshop CC Settings/WorkSpaces/"
+fi
