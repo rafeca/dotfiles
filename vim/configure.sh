c3760d604450744f53a2b1bab885770ffd0879d9
Rafael de Oleza <rafeca@gmail.com>
2015-01-11 22:56:45 +0100

    Install vim vundles after setup

--- vim/configure.sh
+++ vim/configure.sh
@@ -8,3 +8,6 @@ mkdir -p ~/.vim/bundle
 # Configure pathogen and vundle
 cp "`pwd`/$(dirname $0)/pathogen.vim" ~/.vim/autoload
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+# Install vundle plugins
+vim +PluginInstall +qall
