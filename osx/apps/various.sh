cb9394f22fef2be493822637352c4ace17c1e1a0
Rafael de Oleza <rafeca@gmail.com>
2015-06-20 14:57:02 +0200

    Update minecraft paths

--- osx/apps/various.sh
+++ osx/apps/various.sh
@@ -17,10 +17,10 @@ brew cask install telegram
 
 brew cask install minecraft
 
-mkdir -p "$HOME/Documents/random/minecraft saves"
+mkdir -p "$HOME/Documents/random/minecraft/saves"
 mkdir -p "$HOME/Library/Application Support/minecraft"
 
-ln -s "$HOME/Documents/random/minecraft saves" "$HOME/Library/Application Support/minecraft/saves"
+ln -s "$HOME/Documents/random/minecraft/saves" "$HOME/Library/Application Support/minecraft/saves"
 
 # Wacom tablet drivers
 ###########
