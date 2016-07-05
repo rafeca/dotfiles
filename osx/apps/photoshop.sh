9404563a555f6be81f8d4e334e46d332e7887f3d
Rafael de Oleza <rafeca@fb.com>
2016-07-05 21:57:15 +0100

    Add photoshop CC 2015 config file

--- osx/apps/photoshop.sh
+++ osx/apps/photoshop.sh
@@ -2,18 +2,20 @@
 
 if [ -d "$HOME/Library/Preferences/Adobe Photoshop CS5.1 Settings" ]
 then
-  # Link CS5 Workspace
   ln -s "$(pwd)/$(dirname $0)/photoshop/rafeca workspace CS5" "$HOME/Library/Preferences/Adobe Photoshop CS5.1 Settings/WorkSpaces/"
 fi
 
 if [ -d "$HOME/Library/Preferences/Adobe Photoshop CS6 Settings" ]
 then
-  # Link CS6 Workspace
   ln -s "$(pwd)/$(dirname $0)/photoshop/rafeca workspace CS6" "$HOME/Library/Preferences/Adobe Photoshop CS6 Settings/WorkSpaces/"
 fi
 
 if [ -d "$HOME/Library/Preferences/Adobe Photoshop CC Settings" ]
 then
-  # Link CC Workspace
   ln -s "$(pwd)/$(dirname $0)/photoshop/rafeca workspace CS6" "$HOME/Library/Preferences/Adobe Photoshop CC Settings/WorkSpaces/"
+fi
+
+if [ -d "$HOME/Library/Preferences/Adobe Photoshop CC 2015 Settings" ]
+then
+  ln -s "$(pwd)/$(dirname $0)/photoshop/rafeca workspace CC 2015.psw" "$HOME/Library/Preferences/Adobe Photoshop CC 2015 Settings/WorkSpaces/"
 fi
