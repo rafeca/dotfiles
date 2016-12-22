f460522b7f9d4a5a24dc2d10596f80b03cace23b
Rafael de Oleza <rafeca@fb.com>
2016-12-22 21:54:14 +0000

    Add bin folder in Documents

--- zsh/configure.sh
+++ zsh/configure.sh
@@ -11,6 +11,7 @@ echo "source `pwd`/$(dirname $0)/main.zsh" >> ~/.zshrc
 mkdir -p ~/bin
 mkdir -p ~/tmp
 
+mkdir -p ~/Documents/bin
 mkdir -p ~/Documents/work/opensource
 mkdir -p ~/Documents/buro
 mkdir -p ~/Documents/random
--- zsh/path.zsh
+++ zsh/path.zsh
@@ -2,4 +2,5 @@ export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin
 export PATH=/usr/local/bin:$PATH
 export PATH="$(greadlink --canonicalize $(dirname $0)/../bin):$PATH"
 export PATH=~/bin:$PATH
+export PATH=~/Documents/bin:$PATH
 export PATH=~/.local/bin:$PATH
