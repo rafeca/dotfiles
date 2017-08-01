ef664d3e90619d4bce19ec9fda886bd23e7a6b3d
Rafael de Oleza <rafeca@fb.com>
2017-08-01 21:00:26 +0100

    Silence bell

--- osx/apps/iterm2.sh
+++ osx/apps/iterm2.sh
@@ -30,6 +30,9 @@ defaults write com.googlecode.iterm2 ShowFullScreenTabBar -bool true
 # Unlimited Scrollback
 /usr/libexec/PlistBuddy -c "Set 'New Bookmarks':0:'Unlimited Scrollback' true" ~/Library/Preferences/com.googlecode.iTerm2.plist
 
+# Mute bell
+/usr/libexec/PlistBuddy -c "Set 'New Bookmarks':0:'Silence Bell' true" ~/Library/Preferences/com.googlecode.iTerm2.plist
+
 # Import Solarized Theme
 /usr/libexec/PlistBuddy -c "Add 'Custom Color Presets':'Solarized Dark' dict" ~/Library/Preferences/com.googlecode.iTerm2.plist
 /usr/libexec/PlistBuddy -c "Merge '$(pwd)/$(dirname $0)/iterm2/Solarized Dark.itermcolors' 'Custom Color Presets':'Solarized Dark'" ~/Library/Preferences/com.googlecode.iTerm2.plist
