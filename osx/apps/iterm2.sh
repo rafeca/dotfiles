858396b58055bcce72c7e2b0ebbc23e95a4eea08
Rafael Oleza <rafeca@gmail.com>
2020-06-05 11:46:39 +0200

    Remove applying theme to iTerm
    This generates a corrupted plist file

--- osx/apps/iterm2.sh
+++ osx/apps/iterm2.sh
@@ -43,23 +43,6 @@ defaults write com.googlecode.iterm2 ShowFullScreenTabBar -bool true
 /usr/libexec/PlistBuddy -c "Add 'Custom Color Presets':'Base16 Chalk Dark' dict" ~/Library/Preferences/com.googlecode.iTerm2.plist
 /usr/libexec/PlistBuddy -c "Merge '$(pwd)/$(dirname $0)/iterm2/Base16 Chalk Dark.itermcolors' 'Custom Color Presets':'Base16 Chalk Dark'" ~/Library/Preferences/com.googlecode.iTerm2.plist
 
-# Apply Solarized Theme in default profile
-for color in \
-  "Ansi 0 Color" "Ansi 1 Color" "Ansi 2 Color" "Ansi 3 Color" "Ansi 4 Color" \
-  "Ansi 5 Color" "Ansi 6 Color" "Ansi 7 Color" "Ansi 8 Color" "Ansi 9 Color" \
-  "Ansi 10 Color" "Ansi 11 Color" "Ansi 12 Color" "Ansi 13 Color" "Ansi 14 Color" \
-  "Ansi 15 Color" "Background Color" "Bold Color" "Cursor Color" "Cursor Text Color" \
-  "Foreground Color" "Selected Text Color" "Selection Color"; do
-
-  /usr/libexec/PlistBuddy -c "Delete :'New Bookmarks':0:'$color'" ~/Library/Preferences/com.googlecode.iterm2.plist
-done
-/usr/libexec/PlistBuddy -c "Merge '$(pwd)/$(dirname $0)/iterm2/Base16 Chalk Dark.itermcolors' 'New Bookmarks':0" ~/Library/Preferences/com.googlecode.iterm2.plist
-
-# Set ANSI black color bright to 74,74,74 (needed for Solarized Dark)
-# /usr/libexec/PlistBuddy -c "Set 'New Bookmarks':0:'Ansi 8 Color':'Blue Component' 74" ~/Library/Preferences/com.googlecode.iterm2.plist
-# /usr/libexec/PlistBuddy -c "Set 'New Bookmarks':0:'Ansi 8 Color':'Red Component' 74" ~/Library/Preferences/com.googlecode.iterm2.plist
-# /usr/libexec/PlistBuddy -c "Set 'New Bookmarks':0:'Ansi 8 Color':'Green Component' 74" ~/Library/Preferences/com.googlecode.iterm2.plist
-
 # Add shortcut to Dock
 defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/iTerm.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
 
