816cc8b23fb1d8fd214994b59a6ddf8a2da19276
Rafael Oleza <rafeca@gmail.com>
2020-06-05 11:48:18 +0200

    Better setup of git gpg signing

--- git/configure.sh
+++ git/configure.sh
@@ -3,6 +3,9 @@
 # Install latest version of git
 brew install git
 
+# Install gpg-suite to be able to store gpg keys passphrases into OSX Keychain
+brew cask install gpg-suite
+
 # Get full name of the OSX user (cheers @juandebravo!)
 username=`whoami`
 default_git_name="`finger $USER | head -n1 | cut -d ":" -f 3 | cut -c 2-`"
@@ -19,3 +22,18 @@ echo "    email = $git_email" >> ~/.gitconfig.credentials
 
 ln -s "`pwd`/$(dirname $0)/gitconfig.symlink" ~/.gitconfig
 ln -s "`pwd`/$(dirname $0)/gitignore.symlink" ~/.gitignore
+
+## Print instructions to setup gpg signing
+
+echo "Please run the following commands to setup gpg signing:"
+echo ""
+echo "$ gpg --full-generate-key"
+echo ""
+echo "Look for a string similar to \"gpg: key <KEY_ID> marked as ultimately trusted\""
+echo "on the command output and use that KEY_ID in the following command:"
+echo ""
+echo "$ gpg --armor --export <KEY_ID> | pbcopy"
+echo ""
+echo "Now go to https://github.com/settings/keys and create a new GPG key with the clipboard contents."
+echo ""
+echo "Last step is adding a line with \"signingkey = <KEY_ID>\" at the end of the ~/.gitconfig.credentials file"
