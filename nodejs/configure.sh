e2209554bfe02f896d9ae860b9af494721d016de
Rafael Oleza <rafeca@gmail.com>
2020-06-05 11:42:34 +0200

    Update nodejs setup steps

--- nodejs/configure.sh
+++ nodejs/configure.sh
@@ -6,7 +6,7 @@ then
   echo "Installing NVM..."
 
   # Install nvm:
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
   # Configure nvm in the ~/.zshrc file:
   echo "source `pwd`/$(dirname $0)/nodejs.zsh" >> ~/.zshrc
@@ -16,17 +16,22 @@ fi
 
 # Install latest nodejs v8 version
 . ~/.nvm/nvm.sh
-nvm install 8
-nvm alias default 8
+nvm install 12
+nvm alias default 12
+
+# Install yarn
+brew install yarn
 
 # Install some global modules
-npm install -g node-static prettyjson grunt-cli gulp eslint yarn
+yarn global add node-static prettyjson eslint
 
-if [ "Not authed.  Run 'npm adduser'" = "$(npm whoami)" ]
-then
+# Login to npm if we're not logged in yet.
+npm whoami >> /dev/null 2>&1
+exit_status=$?
+if [ $exit_status -eq 1 ]; then
   # Login into npm (for publishing)
   npm login
 fi
 
-# Add jshint default config
+# Add eslint default config
 ln -s "`pwd`/$(dirname $0)/eslintrc.symlink" ~/.eslintrc
