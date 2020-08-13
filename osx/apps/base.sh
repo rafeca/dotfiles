7cf3d6869f82e00fef160489f988d52ed31b02b3
Rafael Oleza <rafeca@gmail.com>
2020-08-13 11:39:37 +0200

    Install all base apps automatically

--- osx/apps/base.sh
+++ osx/apps/base.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-osx/apps/base/alfred.sh
-osx/apps/base/caffeine.sh
-osx/apps/base/chrome.sh
-osx/apps/base/cyberduck.sh
-osx/apps/base/gpg-utils.sh
-osx/apps/base/iterm2.sh
-osx/apps/base/osxfuse.sh
-osx/apps/base/quicklookplugins.sh
-osx/apps/base/prefs-editor.sh
-osx/apps/base/slate.sh
-osx/apps/base/spotify.sh
-osx/apps/base/the-unarchiver.sh
-osx/apps/base/transmission.sh
-osx/apps/base/videolan.sh
-osx/apps/base/whatsapp.sh
+for filename in $(dirname "$0")/cuenca/*.sh; do
+    $filename
+done
